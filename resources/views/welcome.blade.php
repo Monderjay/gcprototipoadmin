f122c7fc226c41e5aed404b6db4724d023da23eb
Eduardo Chavez <pank9605@hotmail.com>
2020-06-20 18:41:55 -0500

    image list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -96,7 +96,7 @@
                     <ul class="list-group list-group-flush aside-list">
                         @foreach($moreContent as $item)
                             <li class="list-group-item d-flex justify-content-start align-items-center elegant-color border-light">
-                                <img class="" src="{{$item->news_image_featured}}"  srcset="{{$item->news_image_featured}} 1000w , {{$item->news_image_featured_medium}} 500w , {{$item->news_image_featured_small}} 360w" sizes="40px" alt="{{$item->title}}">
+                                <img class="" src="{{$item->news_image_featured}}"  srcset="{{$item->news_image_featured}} 900w , {{$item->news_image_featured_medium}} 500w , {{$item->news_image_featured_small}} 300w" sizes="40px" alt="{{$item->title}}">
                                 <div class="mr-2 ml-2 text-left"><a href="{{$item->slug}}" class="white-text links"><div>{{$item->title}}</div></a></div>
                             </li>
                         @endforeach
