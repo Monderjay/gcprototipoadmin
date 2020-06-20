203f310615faf99c4a5ab480a4278c7357cfc301
Eduardo Chavez <pank9605@hotmail.com>
2020-06-20 00:09:41 -0500

    pruba tamaño imagenes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -67,16 +67,17 @@
             </div>
 
 
-            <div class="col-12 col-xl-3 pl-0 pl-xl-3 pr-0 pt-0 mb-4 d-none">
+            <div class="col-12 col-xl-3 pl-0 pl-xl-3 pr-0 pt-0 mb-4">
 
                 <div class="pt-4 pb-4 elegant-color">
                     <div class="title white-text text-center mb-3"><h4><i class="fas fa-signature"></i> Ultimas Reseñas</h4></div>
                     <ul class="list-group list-group-flush aside-list pr-3 pl-3">
                         @foreach($reviewSection as $item)
                             <li class="list-group-item d-flex justify-content-between align-items-center elegant-color border-light">
-                                <div class="image-list-container">
-                                    <img class="" src="{{$item->news_image_featured}}" alt="{{$item->title}}">
+                                <div class="" style="width:50px; height: 50px">
+                                    <img class="img-fluid" src="{{$item->news_image_featured}}" alt="{{$item->title}}">
                                 </div>
+
                                 <div class="mr-2 ml-2 text-left">
                                     <a href="{{$item->slug}}" class="white-text links"><div>{{$item->title}}</div></a>
                                 </div>
@@ -94,7 +95,7 @@
                     </ul>
                 </div>
 
-                <div class="mt-4 pt-4 pb-4 elegant-color">
+                <div class="mt-4 pt-4 pb-4 elegant-color d-none">
                     <div class="title white-text text-center mb-3"><h4><i class="fas fa-plus-square"></i> Más Contenido</h4></div>
                     <ul class="list-group list-group-flush aside-list">
                         @foreach($moreContent as $item)
@@ -106,7 +107,7 @@
                     </ul>
                 </div>
 
-                <div class="mt-4 pt-4 pb-4 elegant-color">
+                <div class="mt-4 pt-4 pb-4 elegant-color d-none">
                     <div class="title white-text text-center mb-3"><h4><i class="fas fa-rocket"></i> Retro</h4></div>
                     <ul class="list-group list-group-flush aside-list pr-3 pl-3">
                         @foreach($retroContent as $item)
