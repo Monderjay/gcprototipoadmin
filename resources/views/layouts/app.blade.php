2891f2f4577e16ccdf127680b5f7595729aafef1
Eduardo Chavez <pank9605@hotmail.com>
2020-06-23 18:16:37 -0500

    ajustes de carga principal

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,9 @@
     <link rel="shortcut icon" href="{{asset('img/coreblack.webp')}}">
 
     <!-- Styles -->
-    <link rel="preload" as="style" href="{{ asset('css/app.css') }}">
+    <link rel="preload" href="{{ asset('css/app.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
     <link rel="stylesheet" href="{{ asset('css/app-styles.css') }}">
-    <link rel="preload" as="style" href="{{ asset('css/mdb.min.css') }}">
+    <link rel="preload" as="style" href="{{ asset('css/mdb.min.css') }}" onload="this.onload=null;this.rel='stylesheet'">
 
     <!-- Google -->
     <meta name="google-site-verification" content="ZPwFaoBJbJDZczfpo4Nlj52IXf40sJlsg3QSoyngE-A">
