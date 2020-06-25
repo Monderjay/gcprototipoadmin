c59ed93365be101d9efd8be6e0b30389ba042149
Eduardo Chavez <pank9605@hotmail.com>
2020-06-25 13:46:01 -0500

    Ajustes en ckeditor

---
+++ public/js/Knob/config.js
@@ -0,0 +1,26 @@
+$(function() {
+    $(".dial").knob({
+        'min':0,
+        'max':100,
+        'width':43,
+        'height':43,
+        'readOnly':true,
+    });
+
+    var showCalification = $(".show-calification").val();
+    var color ="";
+    if (showCalification < 50 ){
+        color="#ed4757";
+    }else if (showCalification < 80){
+        color="#fdc51b";
+    }else{
+        color="#87ceeb";
+    }
+
+    $(".show-calification").knob({
+        'width':180,
+        'height':180,
+        'readOnly':true,
+        'fgColor':color
+    });
+});
