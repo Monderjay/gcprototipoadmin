ba23202aad28ca63abae38b6556ef306c37ad64c
Eduardo Chavez <pank9605@hotmail.com>
2020-06-27 21:08:59 -0500

    google configuration

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,17 +16,6 @@
     <meta name="keywords" content="@yield('keywords','game, core, gamers, playstation, xbox, nintendo,pc, móvil, videojuegos, noticas, reseñas')"/>
     <meta name="copyright" content="GameCore"/>
 
-
-    <!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KJW82NQ');</script>
-    <!-- End Google Tag Manager -->
-
-
-
     <!-- Icono -->
     <link rel="shortcut icon" href="{{asset('img/coreblack.webp')}}">
 
